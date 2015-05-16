244a527bab0d9885bc3c039e1921a5085b136ff6
Tobias Gesellchen <tobias@gesellix.de>
2015-05-16 23:59:49 +0200

    extract exporter declaration and factory from collector implementation

--- couchdb-exporter.go
+++ couchdb-exporter.go
@@ -1,177 +1,26 @@
 package main
 
 import (
-	"crypto/tls"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"sync"
 
 	"github.com/golang/glog"
 	"github.com/prometheus/client_golang/prometheus"
-)
-
-const (
-	namespace = "couchdb"
 )
 
 var (
 	listeningAddress = flag.String("telemetry.address", "localhost:9984", "Address on which to expose metrics.")
 	metricsEndpoint = flag.String("telemetry.endpoint", "/metrics", "Path under which to expose metrics.")
 	couchdbURI = flag.String("couchdb.uri", "http://localhost:5984", "URI to the CouchDB instance")
-	insecure = flag.Bool("insecure", true, "Ignore server certificate if using https")
 )
 
 var (
 	exposedHttpStatusCodes = []string{"200", "201", "202", "301", "304", "400", "401", "403", "404", "405", "409", "412", "500"}
 )
-
-// Exporter collects couchdb stats from the given URI and exports them using
-// the prometheus metrics package.
-type Exporter struct {
-	URI    string
-	client *http.Client
-	mutex  sync.RWMutex
-
-	up prometheus.Gauge
-
-	authCacheHits   prometheus.Gauge
-	authCacheMisses prometheus.Gauge
-	databaseReads   prometheus.Gauge
-	databaseWrites  prometheus.Gauge
-	openDatabases   prometheus.Gauge
-	openOsFiles     prometheus.Gauge
-	requestTime     prometheus.Gauge
-
-	httpdStatusCodes    *prometheus.GaugeVec
-	httpdRequestMethods *prometheus.GaugeVec
-
-	clientsRequestingChanges prometheus.Gauge
-	temporaryViewReads       prometheus.Gauge
-	requests                 prometheus.Gauge
-	bulkRequests             prometheus.Gauge
-	viewReads                prometheus.Gauge
-}
-
-// NewExporter returns an initialized Exporter.
-func NewExporter(uri string) *Exporter {
-	couchDbClient := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: *insecure},
-		},
-	}
-	return &Exporter{
-		URI:    fmt.Sprintf("%s/_stats", uri),
-		client: couchDbClient,
-
-		up: prometheus.NewGauge(
-			prometheus.GaugeOpts{
-				Namespace: namespace,
-				Name:      "up",
-				Help:      "Was the last query of CouchDB stats successful.",
-			}),
-
-		authCacheHits: prometheus.NewGauge(
-			prometheus.GaugeOpts{
-				Namespace: namespace,
-				Name:      "auth_cache_hits",
-				Help:      "number of authentication cache hits",
-			}),
-		authCacheMisses: prometheus.NewGauge(
-			prometheus.GaugeOpts{
-				Namespace: namespace,
-				Name:      "auth_cache_misses",
-				Help:      "number of authentication cache misses",
-			}),
-		databaseReads: prometheus.NewGauge(
-			prometheus.GaugeOpts{
-				Namespace: namespace,
-				Name:      "database_reads",
-				Help:      "number of times a document was read from a database",
-			}),
-		databaseWrites: prometheus.NewGauge(
-			prometheus.GaugeOpts{
-				Namespace: namespace,
-				Name:      "database_writes",
-				Help:      "number of times a database was changed",
-			}),
-		openDatabases: prometheus.NewGauge(
-			prometheus.GaugeOpts{
-				Namespace: namespace,
-				Name:      "open_databases",
-				Help:      "number of open databases",
-			}),
-		openOsFiles: prometheus.NewGauge(
-			prometheus.GaugeOpts{
-				Namespace: namespace,
-				Name:      "open_os_files",
-				Help:      "number of file descriptors CouchDB has open",
-			}),
-		requestTime: prometheus.NewGauge(
-			prometheus.GaugeOpts{
-				Namespace: namespace,
-				Name:      "request_time",
-				Help:      "length of a request inside CouchDB without MochiWeb",
-			}),
-
-		httpdStatusCodes: prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Namespace: namespace,
-				Subsystem: "httpd",
-				Name:      "status_codes",
-				Help:      "number of HTTP responses by status code",
-			},
-			[]string{"code"}),
-		httpdRequestMethods: prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Namespace: namespace,
-				Subsystem: "httpd",
-				Name:      "request_methods",
-				Help:      "number of HTTP requests by method",
-			},
-			[]string{"method"}),
-
-
-		bulkRequests: prometheus.NewGauge(
-			prometheus.GaugeOpts{
-				Namespace: namespace,
-				Subsystem: "httpd",
-				Name:      "bulk_requests",
-				Help:      "number of bulk requests",
-			}),
-		clientsRequestingChanges: prometheus.NewGauge(
-			prometheus.GaugeOpts{
-				Namespace: namespace,
-				Subsystem: "httpd",
-				Name:      "clients_requesting_changes",
-				Help:      "number of clients for continuous _changes",
-			}),
-		requests: prometheus.NewGauge(
-			prometheus.GaugeOpts{
-				Namespace: namespace,
-				Subsystem: "httpd",
-				Name:      "requests",
-				Help:      "number of HTTP requests",
-			}),
-		temporaryViewReads: prometheus.NewGauge(
-			prometheus.GaugeOpts{
-				Namespace: namespace,
-				Subsystem: "httpd",
-				Name:      "temporary_view_reads",
-				Help:      "number of temporary view reads",
-			}),
-		viewReads: prometheus.NewGauge(
-			prometheus.GaugeOpts{
-				Namespace: namespace,
-				Subsystem: "httpd",
-				Name:      "view_reads",
-				Help:      "number of view reads",
-			}),
-	}
-}
 
 // Describe describes all the metrics ever exported by the couchdb exporter. It
 // implements prometheus.Collector.
---
+++ exporter.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"net/http"
+	"sync"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+const (
+	namespace = "couchdb"
+)
+
+var (
+	insecure = flag.Bool("insecure", true, "Ignore server certificate if using https")
+)
+
+// Exporter collects couchdb stats from the given URI and exports them using
+// the prometheus metrics package.
+type Exporter struct {
+	URI    string
+	client *http.Client
+	mutex  sync.RWMutex
+
+	up prometheus.Gauge
+
+	authCacheHits   prometheus.Gauge
+	authCacheMisses prometheus.Gauge
+	databaseReads   prometheus.Gauge
+	databaseWrites  prometheus.Gauge
+	openDatabases   prometheus.Gauge
+	openOsFiles     prometheus.Gauge
+	requestTime     prometheus.Gauge
+
+	httpdStatusCodes    *prometheus.GaugeVec
+	httpdRequestMethods *prometheus.GaugeVec
+
+	clientsRequestingChanges prometheus.Gauge
+	temporaryViewReads       prometheus.Gauge
+	requests                 prometheus.Gauge
+	bulkRequests             prometheus.Gauge
+	viewReads                prometheus.Gauge
+}
+
+// NewExporter returns an initialized Exporter.
+func NewExporter(uri string) *Exporter {
+	couchDbClient := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: *insecure},
+		},
+	}
+	return &Exporter{
+		URI:    fmt.Sprintf("%s/_stats", uri),
+		client: couchDbClient,
+
+		up: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Name:      "up",
+				Help:      "Was the last query of CouchDB stats successful.",
+			}),
+
+		authCacheHits: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Name:      "auth_cache_hits",
+				Help:      "number of authentication cache hits",
+			}),
+		authCacheMisses: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Name:      "auth_cache_misses",
+				Help:      "number of authentication cache misses",
+			}),
+		databaseReads: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Name:      "database_reads",
+				Help:      "number of times a document was read from a database",
+			}),
+		databaseWrites: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Name:      "database_writes",
+				Help:      "number of times a database was changed",
+			}),
+		openDatabases: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Name:      "open_databases",
+				Help:      "number of open databases",
+			}),
+		openOsFiles: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Name:      "open_os_files",
+				Help:      "number of file descriptors CouchDB has open",
+			}),
+		requestTime: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Name:      "request_time",
+				Help:      "length of a request inside CouchDB without MochiWeb",
+			}),
+
+		httpdStatusCodes: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Subsystem: "httpd",
+				Name:      "status_codes",
+				Help:      "number of HTTP responses by status code",
+			},
+			[]string{"code"}),
+		httpdRequestMethods: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Subsystem: "httpd",
+				Name:      "request_methods",
+				Help:      "number of HTTP requests by method",
+			},
+			[]string{"method"}),
+
+
+		bulkRequests: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Subsystem: "httpd",
+				Name:      "bulk_requests",
+				Help:      "number of bulk requests",
+			}),
+		clientsRequestingChanges: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Subsystem: "httpd",
+				Name:      "clients_requesting_changes",
+				Help:      "number of clients for continuous _changes",
+			}),
+		requests: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Subsystem: "httpd",
+				Name:      "requests",
+				Help:      "number of HTTP requests",
+			}),
+		temporaryViewReads: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Subsystem: "httpd",
+				Name:      "temporary_view_reads",
+				Help:      "number of temporary view reads",
+			}),
+		viewReads: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Subsystem: "httpd",
+				Name:      "view_reads",
+				Help:      "number of view reads",
+			}),
+	}
+}

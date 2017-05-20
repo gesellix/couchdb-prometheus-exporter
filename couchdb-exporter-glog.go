742480c6952b1ed3d131188d4c4ad10f2ec9f593
Tobias Gesellchen <tobias@gesellix.de>
2017-05-20 23:39:18 +0200

    Add configuration support via environment.
    For backward compatibility, we have to keep the dot `.` in env variables, e.g. `COUCHDB.URI`. Some shells like bash only allow to set such variables like this: `env "COUCHDB.URI=http://localhost:4556" ./couchdb-exporter`.
    
    Sadly, the glog package doesn't work well with custom flag packages. So we needed to manually pass some of the more popular glog flags to glog.

---
+++ couchdb-exporter-glog.go
@@ -0,0 +1,96 @@
+// Idea taken from https://github.com/kubernetes/kubernetes/pull/3342
+
+package main
+
+import (
+	"github.com/golang/glog"
+	"strconv"
+	"sync/atomic"
+	"strings"
+)
+
+// severity identifies the sort of log: info, warning etc. It also implements
+// the flag.Value interface. The -stderrthreshold flag is of type severity and
+// should be modified only through the flag.Value interface. The values match
+// the corresponding constants in C++.
+type severity int32 // sync/atomic int32
+
+// These constants identify the log levels in order of increasing severity.
+// A message written to a high-severity log file is also written to each
+// lower-severity log file.
+const (
+	infoLog    severity = iota
+	warningLog
+	errorLog
+	fatalLog
+)
+
+var severityName = []string{
+	infoLog:    "INFO",
+	warningLog: "WARNING",
+	errorLog:   "ERROR",
+	fatalLog:   "FATAL",
+}
+
+// get returns the value of the severity.
+func (s *severity) get() severity {
+	return severity(atomic.LoadInt32((*int32)(s)))
+}
+
+// set sets the value of the severity.
+func (s *severity) set(val severity) {
+	atomic.StoreInt32((*int32)(s), int32(val))
+}
+
+// String is part of the flag.Value interface.
+func (s *severity) String() string {
+	return strconv.FormatInt(int64(*s), 10)
+}
+
+// Get is part of the flag.Value interface.
+//func (s *severity) Get() interface{} {
+//	return *s
+//}
+
+// Set is part of the flag.Value interface.
+func (s *severity) Set(value string) error {
+	var threshold severity
+	// Is it a known name?
+	if v, ok := severityByName(value); ok {
+		threshold = v
+	} else {
+		v, err := strconv.Atoi(value)
+		if err != nil {
+			return err
+		}
+		threshold = severity(v)
+	}
+	logging.stderrThreshold.set(threshold)
+	return nil
+}
+
+func severityByName(s string) (severity, bool) {
+	s = strings.ToUpper(s)
+	for i, name := range severityName {
+		if name == s {
+			return severity(i), true
+		}
+	}
+	return 0, false
+}
+
+// loggingT collects all the global state of the logging setup.
+type loggingT struct {
+	// Boolean flags. Not handled atomically because the flag.Value interface
+	// does not let us avoid the =true, and that shorthand is necessary for
+	// compatibility. TODO: does this matter enough to fix? Seems unlikely.
+	toStderr     bool // The -logtostderr flag.
+	alsoToStderr bool // The -alsologtostderr flag.
+
+	// Level flag. Handled atomically.
+	stderrThreshold severity // The -stderrthreshold flag.
+
+	verbosity glog.Level // V logging level, the value of the -v flag/
+}
+
+var logging loggingT
--- couchdb-exporter.go
+++ couchdb-exporter.go
@@ -1,39 +1,67 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	goflag "flag"
 	"net/http"
-
+	"strconv"
 	"github.com/gesellix/couchdb-exporter/lib"
 	"github.com/golang/glog"
+	"github.com/namsral/flag"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-var (
-	listenAddress   = flag.String("telemetry.address", "localhost:9984", "Address on which to expose metrics.")
-	metricsEndpoint = flag.String("telemetry.endpoint", "/metrics", "Path under which to expose metrics.")
-	couchdbURI      = flag.String("couchdb.uri", "http://localhost:5984", "URI to the CouchDB instance")
-	couchdbUsername = flag.String("couchdb.username", "", "Basic auth username for the CouchDB instance")
-	couchdbPassword = flag.String("couchdb.password", "", "Basic auth password for the CouchDB instance")
-)
+type exporterConfigType struct {
+	listenAddress   string
+	metricsEndpoint string
+	couchdbURI      string
+	couchdbUsername string
+	couchdbPassword string
+}
+
+var exporterConfig exporterConfigType
+
+func init() {
+	flag.StringVar(&exporterConfig.listenAddress, "telemetry.address", "localhost:9984", "Address on which to expose metrics.")
+	flag.StringVar(&exporterConfig.metricsEndpoint, "telemetry.endpoint", "/metrics", "Path under which to expose metrics.")
+	flag.StringVar(&exporterConfig.couchdbURI, "couchdb.uri", "http://localhost:5984", "URI to the CouchDB instance")
+	flag.StringVar(&exporterConfig.couchdbUsername, "couchdb.username", "", "Basic auth username for the CouchDB instance")
+	flag.StringVar(&exporterConfig.couchdbPassword, "couchdb.password", "", "Basic auth password for the CouchDB instance")
+
+	flag.BoolVar(&logging.toStderr, "logtostderr", false, "log to standard error instead of files")
+	flag.BoolVar(&logging.alsoToStderr, "alsologtostderr", false, "log to standard error as well as files")
+	flag.Var(&logging.verbosity, "v", "log level for V logs")
+	flag.Var(&logging.stderrThreshold, "stderrthreshold", "logs at or above this threshold go to stderr")
+
+	// Default stderrThreshold is ERROR.
+	logging.stderrThreshold = errorLog
+}
 
 func main() {
 	flag.Parse()
+	// Convinces goflags that we have called Parse() to avoid noisy logs.
+	// Necessary due to https://github.com/golang/glog/commit/65d674618f712aa808a7d0104131b9206fc3d5ad
+	// and us using another flags package.
+	goflag.Parse()
+	goflag.Lookup("logtostderr").Value.Set(strconv.FormatBool(*&logging.toStderr))
+	goflag.Lookup("alsologtostderr").Value.Set(strconv.FormatBool(*&logging.alsoToStderr))
+	goflag.Lookup("v").Value.Set(logging.verbosity.String())
+	goflag.Lookup("stderrthreshold").Value.Set(logging.stderrThreshold.String())
 
-	exporter := lib.NewExporter(*couchdbURI, lib.BasicAuth{Username: *couchdbUsername, Password: *couchdbPassword})
+	exporter := lib.NewExporter(*&exporterConfig.couchdbURI, lib.BasicAuth{Username: *&exporterConfig.couchdbUsername, Password: *&exporterConfig.couchdbPassword})
 	prometheus.MustRegister(exporter)
 
-	http.Handle(*metricsEndpoint, prometheus.Handler())
+	http.Handle(*&exporterConfig.metricsEndpoint, promhttp.Handler())
 	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "OK")
 	})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, *metricsEndpoint, http.StatusMovedPermanently)
+		http.Redirect(w, r, *&exporterConfig.metricsEndpoint, http.StatusMovedPermanently)
 	})
 
-	glog.Infof("Starting exporter at %s to read from CouchDB at %s", *listenAddress, *couchdbURI)
-	err := http.ListenAndServe(*listenAddress, nil)
+	glog.Infof("Starting exporter at %s to read from CouchDB at %s", *&exporterConfig.listenAddress, *&exporterConfig.couchdbURI)
+	err := http.ListenAndServe(*&exporterConfig.listenAddress, nil)
 	if err != nil {
 		glog.Fatal(err)
 	}

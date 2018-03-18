ebce2c95bd806ebf8ecfa86c1a62f15dd269f778
Tobias Gesellchen <tobias@gesellix.de>
2018-03-18 17:17:28 +0100

    fix compose `up` command syntax

--- integration-test.sh
+++ integration-test.sh
@@ -6,7 +6,7 @@ cat << EOB > .couchdb-env
 COUCHDB.USERNAME=root
 COUCHDB.PASSWORD=a-secret
 EOB
-docker-compose -d -f docker-compose-cluster.yml -p db up
+docker-compose -f docker-compose-cluster.yml -p db up -d
 
 curl -X GET "http://localhost:9984/metrics" -v
 

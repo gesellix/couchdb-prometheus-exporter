c0e1c3cc63b23295332ae2c2dbc0441a0891998d
Tobias Gesellchen <tobias@gesellix.de>
2018-03-18 17:22:02 +0100

    enable overlay networks by enabling swarm mode

--- integration-test.sh
+++ integration-test.sh
@@ -6,6 +6,11 @@ cat << EOB > .couchdb-env
 COUCHDB.USERNAME=root
 COUCHDB.PASSWORD=a-secret
 EOB
+
+# We want to use overlay networks to make them `attachable`.
+# Overlay networks are only available in swarm mode, though.
+# See https://docs.docker.com/compose/compose-file/compose-file-v2/#network-configuration-reference
+docker swarm init
 docker-compose -f docker-compose-cluster.yml -p db up -d
 
 curl -X GET "http://localhost:9984/metrics" -v

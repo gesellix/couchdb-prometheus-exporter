ba0a396fd3611cdba40bbfb3e0aeb2c2aeda7748
Tobias Gesellchen <tobias@gesellix.de>
2018-03-26 08:56:15 +0200

    fix integration test setup

--- integrationtest-setup.sh
+++ integrationtest-setup.sh
@@ -7,7 +7,10 @@ set -o errexit
 # We want to use overlay networks to make them `attachable`.
 # Overlay networks are only available in swarm mode, though.
 # See https://docs.docker.com/compose/compose-file/compose-file-v2/#network-configuration-reference
-docker swarm init
+if [ "$(docker info --format '{{ .Swarm.LocalNodeState }}')" == "inactive" ];then
+  echo "Initialize swarm manager."
+  docker swarm init
+fi
 docker-compose -f examples/compose/docker-compose-integrationtest.yml -p db up -d
 
 curl -X GET "http://localhost:9984/metrics" -v

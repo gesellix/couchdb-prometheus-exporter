d50d69f911fc3f502b4e9491381ae9c0046d5726
Tobias Gesellchen <tobias@gesellix.de>
2018-11-20 00:51:21 +0100

    add an exporter for the couchdb 2 cluster in integration tests

--- integrationtest-setup.sh
+++ integrationtest-setup.sh
@@ -7,7 +7,7 @@ set -o errexit
 # We want to use overlay networks to make them `attachable`.
 # Overlay networks are only available in swarm mode, though.
 # See https://docs.docker.com/compose/compose-file/compose-file-v2/#network-configuration-reference
-if [ "$(docker info --format '{{ .Swarm.LocalNodeState }}')" == "inactive" ];then
+if [[ "$(docker info --format '{{ .Swarm.LocalNodeState }}')" == "inactive" ]];then
   echo "Initialize swarm manager."
   docker swarm init
 fi

57e91a1a8b3856fb2ffc005e438bb0fc7a1f16ff
Tobias Gesellchen <tobias@gesellix.de>
2019-08-05 21:55:57 +0200

    Show verbose output in example setup script

--- examples/stack/cluster-config.sh
+++ examples/stack/cluster-config.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+set -o errexit
+set -o nounset
+#set -o xtrace
+
 COUCHDB_ADDRESS="${1:-http://couchdb:5984}"
 CONFIG_PROPERTIES="${2:-config.properties}"
 
@@ -8,14 +12,14 @@ prop() {
     grep "${1}" "${CONFIG_PROPERTIES}" | cut -d'=' -f2
 }
 
-curl -X POST "${COUCHDB_ADDRESS}/_cluster_setup" \
+curl -v -X POST "${COUCHDB_ADDRESS}/_cluster_setup" \
      -H "Content-Type: application/json" \
      -d "{\"action\":\"enable_cluster\", \"username\":\"$(prop couchdb.username)\", \"password\":\"$(prop couchdb.password)\", \"bind_address\":\"0.0.0.0\", \"port\":5984, \"node_count\": 1}"
-curl -X POST "${COUCHDB_ADDRESS}/_session" \
+curl -v -X POST "${COUCHDB_ADDRESS}/_session" \
      -H "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" \
      -d "name=$(prop couchdb.username)&password=$(prop couchdb.password)" \
      --cookie-jar couchdb-cookies.txt
-curl -X POST "${COUCHDB_ADDRESS}/_cluster_setup" \
+curl -v -X POST "${COUCHDB_ADDRESS}/_cluster_setup" \
      -H "Content-Type: application/json" \
      -d '{"action":"finish_cluster"}' \
      --cookie couchdb-cookies.txt

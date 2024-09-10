c0714f98b40ba8aed9a6bde815475ba50ee98962
Tobias Gesellchen <tobias@gesellix.de>
2024-09-10 20:02:25 +0200

    Replace docker-compose with 'docker compose'

--- integrationtest-teardown.sh
+++ integrationtest-teardown.sh
@@ -4,4 +4,4 @@ set -o nounset
 set -o errexit
 #set -o xtrace
 
-docker-compose -f examples/compose/docker-compose-integrationtest.yml -p db down
+docker compose -f examples/compose/docker-compose-integrationtest.yml -p db down
